99208fb8690ad1a5d8f0aec736a3862cabf0edfc
David Roman <romandavid83@gmail.com>
2019-08-15 17:46:27 -0400

    better version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-import Pipeline from "./Pipeline";
+/**
+ * This function runs your pipeline
+ * @param array The first one is for values, then for functions
+ * @returns Whatever your want to return
+ */
+import Promise from "bluebird";
 
-export default Pipeline;
+//  It's that simple
+export default async a => {
+  return Promise.reduce(
+    a.slice(1, a.length),
+    (ac, cv) => {
+      return cv(ac);
+    },
+    a[0]
+  );
+};
---
+++ test/index.test.js
@@ -0,0 +1,24 @@
+import Promise from "bluebird";
+import Pipeline from "../src";
+
+test("Pipeline", async () => {
+  const capitalize = str => str[0].toUpperCase() + str.substring(1);
+  const splitOnSpaces = str => str.trim().split(" ");
+  const getLastOfArr = arr => arr.pop();
+  const toUpper = values => values.toUpperCase();
+  const wait = async values => {
+    await Promise.delay(500);
+    return values;
+  };
+
+  const data = await Pipeline([
+    "david roman",
+    capitalize,
+    wait,
+    splitOnSpaces,
+    getLastOfArr,
+    toUpper
+  ]);
+
+  expect(data).toBe("ROMAN");
+});
